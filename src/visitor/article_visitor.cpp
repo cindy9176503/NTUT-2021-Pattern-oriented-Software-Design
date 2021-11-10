16afd22ff7c8cb7eca2087eb9623e466a366dd2d
110598068 <t110598068@ntut.org.tw>
2021-11-10 20:41:55 +0800

    Midterm_Update

--- src/visitor/article_visitor.cpp
+++ src/visitor/article_visitor.cpp
@@ -1,10 +1,4 @@
 #include "./article_visitor.h"
-#include "./markdown_visitor.h"
 #include "../text.h"
 #include "../list_item.h"
 #include "../paragraph.h"
-
-
-void MarkdownVisitor::visitListItem(ListItem* li) {
-     _result += li->getText(); 
-};
--- src/visitor/markdown_visitor.h
+++ src/visitor/markdown_visitor.h
@@ -1,15 +1,8 @@
 #pragma once
 
-#include <string>
-
-#include "../text.h"
-#include "../list_item.h"
-#include "../paragraph.h"
-#include "article_visitor.h"
-
 class MarkdownVisitor : public ArticleVisitor {
 public:
-    void visitListItem(ListItem* li);
+    void visitListItem(ListItem* li) override{ _result += li->getText(); };
 
     void visitText(Text* t) override{ _result += t->getText(); }
 
