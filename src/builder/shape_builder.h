57604d44e56004fc22b78fa856ea411333875547
110598068 <t110598068@ntut.org.tw>
2021-12-08 23:17:47 +0800

    HW05_Update

--- src/builder/shape_builder.h
+++ src/builder/shape_builder.h
@@ -1,14 +1,51 @@
+#pragma once
+
+#include "../circle.h"
+#include "../rectangle.h"
+#include "../two_dimensional_vector.h"
+#include "../triangle.h"
+#include "../compound_shape.h"
+
+#include <stack>
+
 class ShapeBuilder {
 public:
-    void buildCircle(double radius) {}
+    void buildCircle(double radius) {
+        _shapes.push(new Circle(radius));
+    }
 
-    void buildRectangle(double length, double width) {}
+    void buildRectangle(double length, double width) {
+        _shapes.push(new Rectangle(length, width));
+    }
 
-    void buildTriangle(double x1, double y1, double x2, double y2) {}
+    void buildTriangle(double x1, double y1, double x2, double y2) { 
+        TwoDimensionalVector vec1(x1, y1);
+        TwoDimensionalVector vec2(x2, y2);   
+        _shapes.push(new Triangle(vec1, vec2));
+    }
 
-    void buildCompoundBegin() {}
+    void buildCompoundBegin() {
+        _shapes.push(new CompoundShape());
+    }
 
-    void buildCompoundEnd() {}
+    void buildCompoundEnd() {
+        std::list<Shape*> components;
+        Shape* top = _shapes.top();
+        while(typeid(*top) != typeid(CompoundShape) ||
+             (typeid(*top) == typeid(CompoundShape) && !top->createIterator()->isDone())) {
+            components.push_back(top);
+            _shapes.pop();
+            top = _shapes.top();
+        }
+        for(auto it = components.rbegin() ; it != components.rend() ; it ++){
+            top->addShape(*it);
+        }
+    }
 
-    Shape* getShape() {}
+    Shape* getShape() {
+        return _shapes.top();
+    }
+
+private:
+    std::stack<Shape*> _shapes;
 };
---
+++ test/builder/ut_shape_builder.h
@@ -0,0 +1,68 @@
+#include "../../src/shape.h"
+#include "../../src/builder/shape_builder.h"
+
+TEST(CaseShapeBuilder, BuildCircle) {
+    ShapeBuilder* builder = new ShapeBuilder();
+
+    builder->buildCircle(1.0);
+    Shape* result = builder->getShape();
+
+    ASSERT_NEAR(1*1*M_PI, result->area(), 0.01);
+
+    delete builder, result;
+}
+
+TEST(CaseShapeBuilder, BuildRectangle) {
+    ShapeBuilder* builder = new ShapeBuilder();
+
+    builder->buildRectangle(1.0, 2.0);
+    Shape* result = builder->getShape();
+
+    ASSERT_NEAR(2.0, result->area(), 0.01);
+
+    delete builder, result;
+}
+
+TEST(CaseShapeBuilder, BuildTriangle) {
+    ShapeBuilder* builder = new ShapeBuilder();
+
+    builder->buildTriangle(3.0, 4.0, 3.0, 0.0);
+    Shape* result = builder->getShape();
+
+    ASSERT_NEAR(6.0, result->area(), 0.01);
+
+    delete builder, result;
+}
+
+TEST(CaseShapeBuilder, BuildCompoundShape) {
+    ShapeBuilder* builder = new ShapeBuilder();
+
+    builder->buildCompoundBegin();
+    builder->buildRectangle(1.0, 2.0);
+    builder->buildRectangle(1.0, 2.0);
+    builder->buildCompoundEnd();
+    Shape* result = builder->getShape();
+
+    ASSERT_NEAR(4, result->area(), 0.01);
+
+    delete builder, result;
+}
+
+TEST(CaseBuilder, BuildComplexCompoundShape){
+    ShapeBuilder* builder = new ShapeBuilder();
+
+    builder->buildCompoundBegin();
+    builder->buildCompoundBegin();
+    builder->buildRectangle(1.0, 1.0);
+    builder->buildRectangle(2.0, 2.0);
+    builder->buildCompoundEnd();
+    builder->buildRectangle(3.0, 3.0);
+    builder->buildCompoundEnd();
+    Shape* result = builder->getShape();
+
+    ASSERT_NEAR(5.0, result->createIterator()->currentItem()->area(), 0.01);
+
+    delete builder;
+}
+
+
