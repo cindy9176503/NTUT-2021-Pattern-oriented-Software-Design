398e29f8a74c0f85b4476686a8260bb0b3287f82
110598068 <t110598068@ntut.org.tw>
2021-12-28 22:32:04 +0800

    HW06_Fix memory leaks

--- test/iterator/ut_null_iterator.h
+++ test/iterator/ut_null_iterator.h
@@ -13,29 +13,21 @@ TEST(CaseNullIterator, CurrentItem_Exception) {
     Shape* result;
 
     ASSERT_ANY_THROW(result = it.currentItem());
-
-    delete result;
 }
 
 TEST(CaseNullIterator, First_Exception) {
-    Iterator* it = new NullIterator();
-    ASSERT_ANY_THROW(it->first());
-
-    delete it;
+    NullIterator it;
+    ASSERT_ANY_THROW(it.first());
 }
 
 TEST(CaseNullIterator, Next_Exception) {
-    Iterator* it = new NullIterator();
-    ASSERT_ANY_THROW(it->next());
-
-    delete it;
+    NullIterator it;
+    ASSERT_ANY_THROW(it.next());
 }
 
 TEST(CaseNullIterator, IsDone_True){
-    Iterator* it = new NullIterator();
-    ASSERT_TRUE(it->isDone());
-
-    delete it;
+    NullIterator it;
+    ASSERT_TRUE(it.isDone());
 }
 
 TEST(CaseNullIterator, NullIteratorIsIterator){
